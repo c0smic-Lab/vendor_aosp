4d6c8dbcd0238151f3670745488c49ae3b1c4062
COSMIC <rayavanindra@gmail.com>
2024-09-05 00:06:08 +0530

    build: Move around some props

--- config/version.mk
+++ config/version.mk
@@ -11,5 +11,12 @@ CUSTOM_PLATFORM_VERSION := 14.0
 CUSTOM_VERSION := cAOSP_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := fourteen
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.device=$(CUSTOM_BUILD) \
+    ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.version=$(CUSTOM_VERSION) \
+    ro.modversion=$(CUSTOM_VERSION)
+
 # Signing
 -include vendor/lineage-priv/keys/keys.mk
