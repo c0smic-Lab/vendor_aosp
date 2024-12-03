9ed1837636a3f0b1318b2d71c27b43341bfde7e5
LuK1337 <priv.luk@gmail.com>
2024-12-03 23:06:13 +0100

    common: Build Camelot
    Change-Id: I823aa9cbaef3fcd1b5503d161017078b3e65c737

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,6 +3,10 @@ $(call inherit-product, vendor/aosp/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
+# Apps
+PRODUCT_PACKAGES += \
+    Camelot
+
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
     unrar \
