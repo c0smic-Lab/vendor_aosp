b8a1a18dc7c2c3bd940c41cec59fcd672c63ad81
Michael Bestas <mkbestas@lineageos.org>
2025-02-10 17:22:44 +0200

    config: Long live ro.modversion
    Change-Id: I4acb0253ee8304c6ab2c3719e1d802e7f5fb6dbf

--- config/version.mk
+++ config/version.mk
@@ -9,8 +9,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
-    ro.custom.version=$(CUSTOM_VERSION) \
-    ro.modversion=$(CUSTOM_VERSION)
+    ro.custom.version=$(CUSTOM_VERSION)
 
 # Signing
 -include vendor/lineage-priv/keys/keys.mk
