3307148eef8b866ee404a9430e4000a91fbfefa5
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-29 22:19:54 +0545

    Revert "Implement hide gestural navigation hint bar [3/3]"
    This reverts commit fc49547848f4c1d36c3d433f4aa9dc4f3102ce7a and partially reverts commit 77d746d029a37787ffa3e628bf42b750f881d187.
    
    Change-Id: I14ec8bb9fb825a27ae6af458f0a3f19238f0691e

--- config/common.mk
+++ config/common.mk
@@ -86,7 +86,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Overlay
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
+    ro.boot.vendor.overlay.theme=com.google.android.systemui.gxoverlay
 
 # Include font files
 include vendor/aosp/fonts/fonts.mk
@@ -216,7 +216,6 @@ PRODUCT_PACKAGES += \
     CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
-    NavigationBarNoHintOverlay \
     ThemedIconsOverlay 
 
 # TextClassifier
