085b2fc2bfeb5070dd96e94aec3f5ed7094175e3
LuK1337 <priv.luk@gmail.com>
2025-05-11 13:09:52 +0200

    config: Use add_soong_config_namespace/add_soong_config_var macros
    Test: m nothing and compare out/soong/soong.lineage_*.variables
    Change-Id: I802f4f7eb37d520fd74599e2b673a5b2368c5d09

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -15,13 +15,5 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += customVarsPlugin
-
-SOONG_CONFIG_customVarsPlugin :=
-
-define addVar
-  SOONG_CONFIG_customVarsPlugin += $(1)
-  SOONG_CONFIG_customVarsPlugin_$(1) := $($1)
-endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+$(call add_soong_config_namespace,customVarsPlugin)
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,customVarsPlugin,$(v))))
